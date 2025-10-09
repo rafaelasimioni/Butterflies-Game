bfb6204c591a7cd8b96a7a39236ae531a59c5612
Rafaela Simioni <rsimionideavila@gmail.com>
2025-10-09 19:09:19 -0300

    Adding classes generated from startUML

---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window: Surface = None
+        self.name: str = None
+        self.entity_list: list = None
+
+    def run(self, ):
+        pass
